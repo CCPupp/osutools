638f0bcba6c2cc13486facdd4fb094d4db501da7
ShotgunApe <shotgunayyype@gmail.com>
2021-06-15 13:34:31 -0700

    Last modified date + time added

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -12,3 +12,18 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+function req(username) {
+  const xhr = new XMLHttpRequest();
+  const url = `https://api.github.com/users/${username}/repos`;
+
+  xhr.open('GET', url, true);
+
+  xhr.onload = function() {
+      const data = JSON.parse(this.response);
+      console.log(data);
+
+      document.getElementById("lastModified").innerHTML = "Last modified: " + data[5].updated_at.slice(0, 10) + " at " + data[5].updated_at.slice(11, 19) + " utc";
+  }
+  xhr.send();
+}
